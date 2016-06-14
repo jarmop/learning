e6b005ceb168252aff616229ed0b1249066024af
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2016-06-14 08:45:52 +0300

    Initial Commit

---
+++ data-structures-and-algorithms/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  data-structures-and-algorithms
+//
+//  Created by Jarmo Pihlajaniemi on 14/06/16.
+//  Copyright © 2016 Jarmo Pihlajaniemi. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
