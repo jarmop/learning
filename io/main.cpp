cd47d0d9ecb88555cdd15ff8869befaf79a08dbf
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2016-06-20 06:56:48 +0300

    Io examples.

---
+++ io/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string a;
+    cin >> a;
+    cout << a << "\n";
+
+    int b;
+    cin >> b;
+
+    string c;
+    cin >> c;
+
+    cout << b << c << "\n";
+
+    return 0;
+}
+
