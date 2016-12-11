f5ede3bd3109e903c34e49600709c777ef9b70d2
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2016-12-11 10:30:21 +0200

    Another example Google interview question.

--- practice-problems/google-interview.cpp
+++ practice-problems/google-interview.cpp
@@ -1,4 +1,7 @@
 #include <iostream>
+#include <set>
+#include <vector>
+
 using namespace std;
 
 /**
@@ -66,12 +69,93 @@ void onsite3() {
  * Given a list of files, each of which contains some "require METHOD_NAME" lines, and "provide METHOD_NAME" lines. Return a working order of files to be processed.
  */
 
+// First go through each file and find unique methods that are required somewhere
+// Find files where required methods are provided
+// Map each file to the files from where they require a method
+// A B C D E
+
 /**
  * Onsite 5:
  * Implement a telephone # management system that assign randomly, recycle and check availability of 10 digit numbers.
  */
 
+/* Another interview set
+ * https://www.glassdoor.com/Interview/Google-Software-Engineer-Interview-Questions-EI_IE9079.0,6_KO7,24.htm#InterviewReview_12617243
+ * Given an NxN matrix how would you print all elements in a spiral way starting from the middle element.
+ */
+void printOuterRing(int matrix[][4], int start, int end) {
+    cout << "print left side" << endl;
+    int j = start;
+    for (int i = start; i <= end; i++) {
+        cout << matrix[i][j] << endl;
+    }
+    cout << "print bottom side" << endl;
+    int i = end;
+    for (int j = start + 1; j <= end; j++) {
+        cout << matrix[i][j] << endl;
+    }
+    cout << "print right side" << endl;
+    j = end;
+    for (int i = end - 1; i >= start; i--) {
+        cout << matrix[i][j] << endl;
+    }
+    cout << "print top side" << endl;
+    i = start;
+    for (int j = end - 1; j >= start + 1; j--) {
+        cout << matrix[i][j] << endl;
+    }
+}
+
+void phone_screen() {
+    const int n = 4;
+    int matrix[4][4] = {
+            {1,2,3,4},
+            {5,6,7,8},
+            {9,10,11,12},
+            {13,14,15,16}
+    };
+//    int matrix[3][3] = {
+//            {1,2,3},
+//            {4, 5,6},
+//            {7,8,9}
+//    };
+
+    int start;
+    int end;
+    if (n % 2 == 0) {
+        start = n / 2 - 1;
+        end = start + 1;
+    } else {
+        start = n / 2 - 1;
+        end = start + 1;
+    }
+
+    for (start; start >= 0; start--) {
+        printOuterRing(matrix, start, end);
+        end++;
+    }
+}
+
+
+// Q1: Find all pairs of numbers that are equal to a given sum in a given int array
+// n log n
+void onsite2_int1_q1() {
+    vector<int> vec {1,5,3,7,11,5,6,-3,-1};
+    int sum = 8;
+    set<int> found_nums;
+    for (int i = 0; i < vec.size(); i++) {
+        int complement = sum - vec[i];
+        if (found_nums.find(complement) != found_nums.end()) {
+            cout << vec[i] << "+" << complement << endl;
+        } else {
+            found_nums.insert(vec[i]);
+        }
+    }
+}
+
 int main() {
 //    findPairWithSum();
-    onsite3();
+//    onsite3();
+    phone_screen();
+//    onsite2_int1_q1();
 }
--- types.cpp
+++ types.cpp
@@ -2,37 +2,102 @@
 
 using namespace std;
 
+void char_test() {
+    char c; // 1 byte
+    char16_t c16t; // 2 bytes
+    char32_t c32t; // 4 bytes
+    wchar_t wc32t; // 4 bytes
+    signed char signedChar; // 1 byte
+    unsigned char unsignedChar; // 1 byte
 
+    cout << "Sizes:" << endl;
+    cout << sizeof(c) << endl;
+    cout << sizeof(c16t) << endl;
+    cout << sizeof(c32t) << endl;
+    cout << sizeof(wc32t) << endl;
+    cout << sizeof(signedChar) << endl;
+    cout << sizeof(unsignedChar) << endl;
+
+    c = 'a';
+    c16t = 'a';
+    c32t = 'a';
+    wc32t = 'a';
+    signedChar = 'a';
+    unsignedChar = 'a';
+
+    cout << "Initialized with 'a':" << endl;
+    cout << c << endl;
+    cout << c16t << endl;
+    cout << c32t << endl;
+    cout << wc32t << endl;
+    cout << signedChar << endl;
+    cout << unsignedChar << endl;
+
+    c = 97;
+    c16t = 97;
+    c32t = 97;
+    wc32t = 97;
+    signedChar = 97;
+    unsignedChar = 97;
+
+    cout << "Initialize with 97:" << endl;
+    cout << c << endl;
+    cout << c16t << endl;
+    cout << c32t << endl;
+    cout << wc32t << endl;
+    cout << signedChar << endl;
+    cout << unsignedChar << endl;
+}
+
+void integer_test() {
+    short s;
+    int i;
+    long l;
+    long long l2;
+    unsigned short us;
+    unsigned int ui;
+    unsigned long ul;
+    unsigned long long ul2;
+
+    cout << "Sizes (signed):" << endl;
+    cout << sizeof(s) << endl;
+    cout << sizeof(i) << endl;
+    cout << sizeof(l) << endl;
+    cout << sizeof(l2) << endl;
+    cout << sizeof(us) << endl;
+    cout << sizeof(ui) << endl;
+    cout << sizeof(ul) << endl;
+    cout << sizeof(ul2) << endl;
+
+    s = 1;
+    i = 1;
+    l = 1;
+    l2 = 1;
+    us = 1;
+    ui = 1;
+    ul = 1;
+    ul2 = 1;
+
+    cout << "Initialized with 1:" << endl;
+    cout << s << endl;
+    cout << i << endl;
+    cout << l << endl;
+    cout << l2 << endl;
+    cout << us << endl;
+    cout << ui << endl;
+    cout << ul << endl;
+    cout << ul2 << endl;
+
+    cout << "Uninitialized integer is the same as an integer initialized as zero" << endl;
+    int not_initialized;
+    int initialied_as_zero = 0;
+    cout << not_initialized << endl;
+    cout << initialied_as_zero << endl;
+}
 
 int main() {
-    /**
-     * Fundamental datatypes
-     */
+//    char_test();
+//    integer_test();
+    cout << "Left \a Right" << endl;
 
-    bool b; // 2 bits
-    b = true;
-    cout << &b << endl;
-    int g = b;
-    cout << g;
-
-    decltype(nullptr) wat;
-
-    // integers
-    int i; // 2 byte
-    long l; // 4 bytes
-    long long ll; // 8 bytes
-    unsigned int ui;
-
-
-    // characters
-    char c; // 1 byte
-    char16_t c16t;
-    char32_t c32t;
-    wchar_t wc32t;
-    signed char signedChar;
-    unsigned char unsignedChar;
-
-    /*******/
-
-    // strings
 };
