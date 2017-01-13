7db6e75030e402535fb7a5305559327022e8f2a5
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2017-01-13 19:18:55 +0200

    Added filename parameter to run.sh script.

--- asm/x86_64/run.sh
+++ asm/x86_64/run.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
-yasm -f bin boot.asm -o boot.bin
-dd if=boot.bin of=boot.img
+
+if [ -z ${1+x} ]; then
+    echo "Please give filename"
+    exit 1;
+fi
+
+filename=$1
+
+IFS='.' read -a filename_parts <<< "$filename"
+
+file_basename="${filename_parts[0]}"
+
+yasm -f bin $filename -o "$file_basename".bin
+dd if="$file_basename".bin of="$file_basename".img
 VBoxManage startvm boot-test
