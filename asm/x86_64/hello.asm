e61739185c73a3e158569317e72b7cbd6b5e6f1f
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2017-01-14 09:44:45 +0200

    Added explanations for hello.asm. Save file before running from Vim.

--- asm/x86_64/hello.asm
+++ asm/x86_64/hello.asm
@@ -1,18 +1,21 @@
-   mov ax, 0x07c0
-   mov ds, ax
+; move segment selector 0x07c0 in to data segment register       
+    mov ax, 0x07c0   ; ax = 16-bit accumulator
+    mov ds, ax       ; ds = 16-bit data segment register
+;    mov ds, 0x07c0       ; ds = 16-bit data segment register
  
-   mov si, msg
-ch_loop:lodsb
-   or al, al ; zero=end or str
-   jz hang   ; get out
-   mov ah, 0x0E
-   int 0x10
-   jmp ch_loop
+    mov si, msg      ; si = 16-bit source index (source of BIOS print?)
+print_char:
+    lodsb            ; loads string from si in to al
+    or al, al        ; zero if al = 0 (?)
+    jz hang          ; jz = jump if above instruction results in zero (i.e. no characters to print)
+    mov ah, 0x0e     ; 0x0e function number for displaying character 
+    int 0x10         ; Raise interrupt 0x10 (17th interrupt vector), BIOS video service(?)
+                     ; calls function in ah register placed above (0x0e)
+    jmp print_char
  
 hang:
-   jmp hang
+    jmp hang
  
-msg   db 'Hello World', 13, 10, 0
-   times 510-($-$$) db 0
-   db 0x55
-   db 0xAA
+msg db 'Hello World mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmma', 13, 10 ; 13 = carriage return, 10 = new line
+    times 510-($-$$) db 0 ; add extra bytes to make total file size 512B
+    db 0x55, 0xAA         ; magic number to tell (old) BIOS this is a boot sector
