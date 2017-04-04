3b3e2be66eaba1cf6c0c0c2de6f971550411f483
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2017-04-04 23:32:57 +0300

    Build scss and stream results to autoprefixer.

--- js/gulp/gulpfile.js
+++ js/gulp/gulpfile.js
@@ -1,11 +1,13 @@
 const gulp = require('gulp');
+const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', () =>
-gulp.src('src/app.css')
-    .pipe(autoprefixer({
-        browsers: ['last 2 versions'],
-        cascade: false
-    }))
-    .pipe(gulp.dest('dist'))
+    gulp.src('src/app.scss')
+        .pipe(sass())
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions'],
+            cascade: false
+        }))
+        .pipe(gulp.dest('dist'))
 );
