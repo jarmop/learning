c0cd3acb611b9aaede7ac6c536c2f9da5ab8cd65
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2017-04-05 23:53:16 +0300

    Build js.

--- front-end/gulp/gulpfile.js
+++ front-end/gulp/gulpfile.js
@@ -1,6 +1,9 @@
 const gulp = require('gulp');
 const sass = require('gulp-sass');
 const autoprefixer = require('gulp-autoprefixer');
+const babel = require('gulp-babel');
+const uglify = require('gulp-uglify');
+const sourcemaps = require('gulp-sourcemaps');
 
 gulp.task('sass', () =>
     gulp.src('src/app.scss')
@@ -14,4 +17,19 @@ gulp.task('sass', () =>
 
 gulp.task('sass:watch', () =>
     gulp.watch('src/app.scss', ['sass'])
+);
+
+gulp.task('js', () =>
+    gulp.src('src/app.js')
+        .pipe(sourcemaps.init())
+        .pipe(babel({
+            presets: ['es2015']
+        }))
+        .pipe(uglify())
+        .pipe(sourcemaps.write())
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('js:watch', () =>
+    gulp.watch('src/app.js', ['js'])
 );
