113c375f5f639dba6c4e15920c643e8c9d27a1f0
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2017-05-01 16:16:46 +0300

    Build html and vendor js with gulp.

--- front-end/react/yarn/gulpfile.js
+++ front-end/react/yarn/gulpfile.js
@@ -3,6 +3,7 @@ const babel = require('gulp-babel');
 const uglify = require('gulp-uglify');
 const sourcemaps = require('gulp-sourcemaps');
 const browserSync = require('browser-sync').create();
+const concat = require('gulp-concat');
 
 gulp.task('js', () =>
     gulp.src('src/app.js')
@@ -19,30 +20,34 @@ gulp.task('js:watch', () =>
     gulp.watch('src/app.js', ['js'])
 );
 
-// gulp.task('html', () =>
-//     gulp.src('src/index.html')
-//         .pipe(gulp.dest('dist'))
-// );
-//
-// gulp.task('html:watch', () =>
-//     gulp.watch('src/*.html', ['html'])
-// );
+gulp.task('js:vendor', () => {
+    let vendorSrc = [
+        'node_modules/react/dist/react.js',
+        'node_modules/react-dom/dist/react-dom.js'
+    ];
+
+    gulp.src(vendorSrc)
+        .pipe(uglify())
+        .pipe(concat('vendor.js'))
+        .pipe(gulp.dest('dist'))
+});
+
+gulp.task('html', () =>
+    gulp.src('src/index.html')
+        .pipe(gulp.dest('dist'))
+);
+
+gulp.task('html:watch', () =>
+    gulp.watch('src/*.html', ['html'])
+);
 
 gulp.task('browser-sync', () => {
     browserSync.init({
         server: {
-            baseDir: './'
+            baseDir: './dist'
         }
     });
     gulp.watch('dist/*').on('change', browserSync.reload);
-
-    // browserSync.init({
-    //     server: {
-    //         baseDir: './dist'
-    //     }
-    // });
-    // gulp.watch('dist/*').on('change', browserSync.reload);
 });
 
-// gulp.task('default', ['js', 'js:watch', 'html', 'html:watch', 'browser-sync']);
-gulp.task('default', ['js', 'js:watch', 'browser-sync']);
+gulp.task('default', ['js', 'js:vendor', 'js:watch', 'html', 'html:watch', 'browser-sync']);
