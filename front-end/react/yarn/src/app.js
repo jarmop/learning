23c629e0789cc9324742251d31db1c8a33e2e3f9
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2017-05-04 23:52:18 +0300

    React example.

--- front-end/react/yarn/src/app.js
+++ front-end/react/yarn/src/app.js
@@ -1,4 +1,52 @@
+function formatDate(date) {
+    return date.toLocaleDateString();
+}
+
+function Avatar(props) {
+    return (
+        <img className="Avatar"
+             src={props.author.avatarUrl}
+             alt={props.author.name} />
+    );
+}
+
+function UserInfo(props) {
+    return (
+        <div className="UserInfo">
+            <Avatar author={props.author} />
+            <div className="UserInfo-name">
+                {props.author.name}
+            </div>
+        </div>
+    );
+}
+
+function Comment(props) {
+    return (
+        <div className="Comment">
+            <UserInfo author={props.author} />
+            <div className="Comment-text">
+                {props.text}
+            </div>
+            <div className="Comment-date">
+                {formatDate(props.date)}
+            </div>
+        </div>
+    );
+}
+
+const comment = {
+    date: new Date(),
+    text: 'I hope you enjoy learning React!',
+    author: {
+        name: 'Hello Kitty',
+        avatarUrl: 'http://placekitten.com/g/64/64'
+    }
+};
 ReactDOM.render(
-    <h1>Hello, world!</h1>,
+    <Comment
+        date={comment.date}
+        text={comment.text}
+        author={comment.author} />,
     document.getElementById('root')
 );
