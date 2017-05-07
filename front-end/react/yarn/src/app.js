20580539f324fa17408fd7fc6dc80f5afdfd02c5
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2017-05-07 14:01:57 +0300

    React example of a clock. Also experimenting with jQuery and PostScribe within React.

--- front-end/react/yarn/src/app.js
+++ front-end/react/yarn/src/app.js
@@ -1,52 +1,46 @@
-function formatDate(date) {
-    return date.toLocaleDateString();
-}
-
-function Avatar(props) {
-    return (
-        <img className="Avatar"
-             src={props.author.avatarUrl}
-             alt={props.author.name} />
-    );
-}
-
-function UserInfo(props) {
-    return (
-        <div className="UserInfo">
-            <Avatar author={props.author} />
-            <div className="UserInfo-name">
-                {props.author.name}
-            </div>
-        </div>
-    );
-}
-
-function Comment(props) {
-    return (
-        <div className="Comment">
-            <UserInfo author={props.author} />
-            <div className="Comment-text">
-                {props.text}
-            </div>
-            <div className="Comment-date">
-                {formatDate(props.date)}
-            </div>
-        </div>
-    );
-}
-
-const comment = {
-    date: new Date(),
-    text: 'I hope you enjoy learning React!',
-    author: {
-        name: 'Hello Kitty',
-        avatarUrl: 'http://placekitten.com/g/64/64'
+class Clock extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {date: new Date()};
     }
-};
+
+    componentDidMount() {
+        this.timerID = setInterval(
+            () => this.tick(),
+            1000
+        );
+
+        $(document).ready(() => {
+            // $('#ad').html('<script>console.log("test1");</script>');
+            // $('#ad').html('<script>document.write("egerh");</script>');
+        });
+
+        // postscribe('#ad', '<script>console.log("test1");</script>');
+        postscribe('#ad', '<script>document.write("egerh");</script>');
+    }
+
+    componentWillUnmount() {
+        clearInterval(this.timerID);
+    }
+
+    tick() {
+        this.setState({
+            date: new Date()
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Hello, world!</h1>
+                <h2>It is {this.state.date.toLocaleTimeString()}.</h2>
+                <div id="ad">test</div>
+            </div>
+        );
+    }
+}
+
 ReactDOM.render(
-    <Comment
-        date={comment.date}
-        text={comment.text}
-        author={comment.author} />,
+    <Clock />,
     document.getElementById('root')
 );
