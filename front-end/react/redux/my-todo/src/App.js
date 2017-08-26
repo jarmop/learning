3d32ec7df17a8f51f4f2f498519378f1272dc154
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2017-08-26 19:47:20 +0300

    Complete todo app without redux.

--- front-end/react/redux/my-todo/src/App.js
+++ front-end/react/redux/my-todo/src/App.js
@@ -1,21 +1,80 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+    constructor(props) {
+        super(props)
+        this.state = {
+            todos: [
+                'first todo',
+                'second todo',
+            ]
+        }
+    }
+
+    addTodo(text) {
+        let todos = this.state.todos;
+        todos.push(text);
+        this.setState(todos);
+    }
+
+    removeTodo(indexToRemove) {
+        let todos = this.state.todos;
+        todos = todos.filter((todo, index) => index !== indexToRemove);
+        this.setState({'todos': todos});
+    }
+
+    render() {
+        let todoList = this.state.todos.map((todo, index) =>
+            <Todo key={index} index={index} text={todo} onRemove={this.removeTodo.bind(this)}/>
+        );
+
+        return (
+            <div>
+                <TodoForm onSubmit={this.addTodo.bind(this)}/>
+                <div>
+                    <ul>
+                        {todoList}
+                    </ul>
+                </div>
+            </div>
+        );
+    }
 }
+
+class TodoForm extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: 'do it!'
+        }
+    }
+
+    onValueChange(event) {
+        this.setState({value: event.target.value});
+    }
+
+    onSubmit(event) {
+        event.preventDefault();
+        this.props.onSubmit(this.state.value);
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.onSubmit.bind(this)}>
+                <input type="text" value={this.state.value} onChange={this.onValueChange.bind(this)}/>
+                <input type="submit" value="Add todo"/>
+            </form>
+        );
+    }
+}
+
+const Todo = (props) => {
+    return (
+        <li>{props.text} <button onClick={() => props.onRemove(props.index)}>X</button></li>
+    );
+};
+
+
 
 export default App;
--- front-end/react/redux/todos/src/actions/index.spec.js
+++ front-end/react/redux/todos/src/actions/index.spec.js
@@ -1,7 +1,7 @@
 import * as actions from './index'
 
 describe('todo actions', () => {
-  it('addTodo should create ADD_TODO action', () => {
+  it('removeTodo should create ADD_TODO action', () => {
     expect(actions.addTodo('Use Redux')).toEqual({
       type: 'ADD_TODO',
       id: 0,
