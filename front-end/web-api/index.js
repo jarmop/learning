16fb993fe2ca77d694091a1119fb4e7bd09a79f1
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2017-09-29 12:26:27 +0300

    Moved things around a bit. Web API testing.

---
+++ front-end/web-api/index.js
@@ -0,0 +1,36 @@
+let animation = () => {
+    let element = document.getElementById('element');
+
+    let frameCount = 0;
+    const animate = (maxFrames) => {
+        element.style.height = element.clientHeight + 1 + 'px' ;
+        frameCount++;
+        console.log(frameCount);
+
+        if (frameCount < maxFrames) {
+            console.log('requestAnimationFrame');
+            window.requestAnimationFrame(() => animate(maxFrames));
+        }
+    };
+
+    animate(100);
+};
+
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demo() {
+    setTimeout(() => {
+        console.log('after one second');
+
+    }, 1000);
+    console.log('Taking a break...');
+    await sleep(2000);
+    console.log('Two second later');
+}
+
+demo();
+
+
