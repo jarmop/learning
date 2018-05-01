211c9a8627ef10978a7d25d313c1077a132c519b
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-05-01 21:12:31 +0300

    Bundle scss with webpack.

--- front-end/webpack/styles/webpack.config.js
+++ front-end/webpack/styles/webpack.config.js
@@ -1,4 +1,4 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 // var path = require("path");
 
 module.exports = {
@@ -12,12 +12,26 @@ module.exports = {
       template: 'index.html',
     }),
   ],
-  // devServer: {
+  devServer: {
     // contentBase: path.resolve(__dirname, 'dist'),
     // Display only errors to reduce the amount of output.
     //stats: "errors-only",
     // host: process.env.HOST, // Defaults to `localhost`
     // port: process.env.PORT, // Defaults to 8080
-    // open: true, // Open the page in browser
-  // }
+    open: true, // Open the page in browser
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        // include,
+        // exclude,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+    ],
+  },
 };
