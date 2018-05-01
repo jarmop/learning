c8db3431d5b3e4f5f9858cd0c5db54020ba805ee
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-05-01 17:12:05 +0300

    Use HtmlWebpackPlugin with the styles test app.

--- front-end/webpack/styles/webpack.config.js
+++ front-end/webpack/styles/webpack.config.js
@@ -1,4 +1,5 @@
-var path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+// var path = require("path");
 
 module.exports = {
   mode: 'production',
@@ -6,12 +7,17 @@ module.exports = {
   output: {
     filename: 'bundle.js'
   },
-  devServer: {
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'index.html',
+    }),
+  ],
+  // devServer: {
     // contentBase: path.resolve(__dirname, 'dist'),
     // Display only errors to reduce the amount of output.
     //stats: "errors-only",
-    host: process.env.HOST, // Defaults to `localhost`
-    port: process.env.PORT, // Defaults to 8080
-    open: true, // Open the page in browser
-  }
+    // host: process.env.HOST, // Defaults to `localhost`
+    // port: process.env.PORT, // Defaults to 8080
+    // open: true, // Open the page in browser
+  // }
 };
