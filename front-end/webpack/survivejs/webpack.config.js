8c45d15db30e533d2a83b1c79400d4ba9d2c3d4e
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-05-01 20:57:20 +0300

    Bundle css with webpack.

--- front-end/webpack/survivejs/webpack.config.js
+++ front-end/webpack/survivejs/webpack.config.js
@@ -1,4 +1,4 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   devtool: false, // Disable source maps to make output files easier to explore
@@ -7,4 +7,17 @@ module.exports = {
       template: 'index.html',
     }),
   ],
+  devServer: {
+    open: true, // Open the page in browser
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        // include,
+        // exclude,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
