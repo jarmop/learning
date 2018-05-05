7282408c8886323715ab4b758f6559316541c47f
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-05-05 15:32:07 +0300

    Node.js web server: read response from a file.

--- node.js/server.js
+++ node.js/server.js
@@ -1,7 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 //create a server object:
 http.createServer((request, response) => {
-  response.write('Hello World!'); //write a response to the client
-  response.end(); //end the response
+  fs.readFile('index.html', (error, data) => {
+    response.write(data); //write a response to the client
+    response.end(); //end the response
+  });
 }).listen(8080); //the server object listens on port 8080
