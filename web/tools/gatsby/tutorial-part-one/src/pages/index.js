95861fb84e9f44ef6909f150556202e9f2983546
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-05-21 19:46:13 +0300

    Complete Gatsby tutorial part one.

--- web/tools/gatsby/tutorial-part-one/src/pages/index.js
+++ web/tools/gatsby/tutorial-part-one/src/pages/index.js
@@ -1,3 +1,18 @@
 import React from "react"
+import Link from "gatsby-link";
 
-export default () => <div>Hello world!</div>
+export default () => (
+  <div style={{ color: 'tomato' }}>
+    <h1>Hello, Gatsby!</h1>
+    <p>What a world.</p>
+    <img src="https://source.unsplash.com/random/400x200" alt="" />
+    <ul>
+      <li>
+        <Link to="/whatever-page/">Whatever Page</Link>
+      </li>
+      <li>
+        <Link to="/counter/">Counter</Link>
+      </li>
+    </ul>
+  </div>
+);
---
+++ web/tools/gatsby/tutorial-part-one/src/pages/whatever-page.js
@@ -0,0 +1,10 @@
+import React from "react"
+import Link from "gatsby-link";
+
+
+export default () => (
+  <div style={{ color: 'green' }}>
+    <h1>Hello, page 2!</h1>
+    <Link to="/">Go back to the homepage</Link>
+  </div>
+);
