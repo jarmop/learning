227eb775401f6248c2faa09af16ae7ba20a022dd
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-05-22 00:05:58 +0300

    Complete Gatsby tutorial part seven.

--- web/tools/gatsby/tutorial-parts-four-to-seven/src/pages/index.js
+++ web/tools/gatsby/tutorial-parts-four-to-seven/src/pages/index.js
@@ -1,23 +1,29 @@
-import React from "react";
-import g from "glamorous";
+import React from 'react';
+import g from 'glamorous';
+import Link from 'gatsby-link';
 
-import { rhythm } from "../utils/typography";
+import {rhythm} from '../utils/typography';
 
-export default ({ data }) => {
+export default ({data}) => {
   console.log(data);
   return (
       <div>
-        <g.H1 display={"inline-block"} borderBottom={"1px solid"}>
+        <g.H1 display={'inline-block'} borderBottom={'1px solid'}>
           Amazing Pandas Eating Things
         </g.H1>
         <h4>{data.allMarkdownRemark.totalCount} Posts</h4>
-        {data.allMarkdownRemark.edges.map(({ node }) => (
+        {data.allMarkdownRemark.edges.map(({node}) => (
             <div key={node.id}>
-              <g.H3 marginBottom={rhythm(1 / 4)}>
-                {node.frontmatter.title}{" "}
-                <g.Span color="#BBB">— {node.frontmatter.date}</g.Span>
-              </g.H3>
-              <p>{node.excerpt}</p>
+              <Link
+                  to={node.fields.slug}
+                  css={{textDecoration: `none`, color: `inherit`}}
+              >
+                <g.H3 marginBottom={rhythm(1 / 4)}>
+                  {node.frontmatter.title}{' '}
+                  <g.Span color="#BBB">— {node.frontmatter.date}</g.Span>
+                </g.H3>
+                <p>{node.excerpt}</p>
+              </Link>
             </div>
         ))}
       </div>
@@ -34,6 +40,9 @@ export const query = graphql`
           frontmatter {
             title
             date(formatString: "DD MMMM, YYYY")
+          }
+          fields {
+            slug
           }
           excerpt
         }
