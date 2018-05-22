e3841920640212f7c638a76d3970c414b203e045
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-05-22 23:57:32 +0300

    Format css object into a string. Also add commented out example of using jsBeautify (for future reference).

--- web/standards/css/reference/src/code.js
+++ web/standards/css/reference/src/code.js
@@ -1,13 +1,30 @@
 import React, {Component} from 'react';
 import 'prismjs/themes/prism.css';
 import Prism from 'prismjs';
+// import jsBeautify from 'js-beautify';
+// console.log(jsBeautify.css_beautify(".grid { display: grid; color: black; }"));
 
-const cssString = `
-.grid {
-  display: grid;
-  color: black;
+const cssObject = {
+  '.grid': {
+    display: 'grid',
+    color: 'black',
+  },
+  '.cell': {
+    display: 'grid',
+    color: 'black',
+  }
+};
+
+let cssString = '';
+for (let selector in cssObject) {
+  cssString += selector + ' {\n';
+  let block = cssObject[selector];
+  for (let property in block) {
+    let value = block[property];
+    cssString += '  ' + property + ': ' + value + ';\n';
+  }
+  cssString += '}\n';
 }
-`;
 
 class Code extends Component {
   render() {
