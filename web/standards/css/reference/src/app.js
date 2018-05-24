1f6ff05a6290343d52d3987307d52a8ad2254f90
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-05-24 16:14:57 +0300

    Clean html template. Add reference headers. Use responsive widths for grid examples.

--- web/standards/css/reference/src/app.js
+++ web/standards/css/reference/src/app.js
@@ -6,8 +6,7 @@ import Code from './code';
 const styles = {
   grid: {
     display: 'grid',
-    gridTemplateRows: 'repeat(3, 30px)',
-    gridTemplateColumns: 'repeat(3, 60px)',
+    gridTemplateColumns: 'repeat(2, auto)',
     gridGap: '10px',
   },
 };
@@ -15,21 +14,24 @@ const styles = {
 const styles2 = {
   grid: {
     display: 'grid',
-    gridTemplateRows: 'repeat(3, 30px)',
-    gridTemplateColumns: 'repeat(3, 60px)',
+    gridTemplateColumns: 'repeat(3, auto)',
     gridGap: '10px',
   },
   a: {
-    gridColumn: 2,
+    gridColumn: 3,
     gridRow: 3,
-  }
+  },
+  b: {
+    gridColumn: 2,
+    gridRow: 2,
+  },
 };
 
 const styles3 = {
   grid: {
     display: 'grid',
+    gridTemplateColumns: 'repeat(3, auto)',
     gridTemplateRows: 'repeat(3, 30px)',
-    gridTemplateColumns: 'repeat(3, 60px)',
     gridTemplateAreas: `"a a a"
                        "b c c"
                        "b c c"`,
@@ -46,24 +48,54 @@ const styles3 = {
   }
 };
 
+const styles4 = {
+  grid: {
+    display: 'grid',
+    gridTemplateColumns: 'repeat(3, auto)',
+    gridGap: '10px',
+  },
+};
+
 class App extends Component {
   render() {
     return (
         <div className="reference">
+          <h2 className="reference-header">You can do this</h2>
           <div className="reference-item">
             <Code cssObject={styles}/>
           </div>
           <div className="result">
-            <Grid styles={styles}/>
+            <div className="grid" style={styles.grid}>
+              <div className="cell">a</div>
+              <div className="cell">b</div>
+              <div className="cell">c</div>
+            </div>
           </div>
+          <div className="reference-item">
+            <Code cssObject={styles4}/>
+          </div>
+          <div className="result">
+            <div className="grid" style={styles4.grid}>
+              <div className="cell">aaa<br/>aaa</div>
+              <div className="cell">aaaa</div>
+              <div className="cell">aa</div>
+              <div className="cell">a</div>
+              <div className="cell">a</div>
+              <div className="cell">a</div>
+              <div className="cell">a</div>
+              <div className="cell">a</div>
+              <div className="cell">a<br/>a<br/>a</div>
+            </div>
+          </div>
+          <h2 className="reference-header">You can also do this (but why would you)</h2>
           <div className="reference-item">
             <Code cssObject={styles2}/>
           </div>
           <div className="result">
             <div className="grid" style={styles2.grid}>
               <div className="cell" style={styles2.a}>a</div>
-              <div className="cell"></div>
-              <div className="cell"></div>
+              <div className="cell" style={styles2.b}>b</div>
+              <div className="cell">c</div>
             </div>
           </div>
           <div className="reference-item">
