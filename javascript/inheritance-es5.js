048a1623effd9252ceba15d87b0798d1ca30f41e
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-05-28 08:49:54 +0300

    Inheritance in ES5 and ES6.

---
+++ javascript/inheritance-es5.js
@@ -0,0 +1,40 @@
+// define the Person Class
+function Person() {}
+
+Person.prototype.walk = function(){
+  console.log ('I am walking!');
+};
+Person.prototype.sayHello = function(){
+  console.log ('hello');
+};
+
+// define the Student class
+function Student() {
+  // Call the parent constructor
+  Person.call(this);
+}
+
+// inherit Person
+Student.prototype = Object.create(Person.prototype);
+
+// correct the constructor pointer because it points to Person
+Student.prototype.constructor = Student;
+
+// replace the sayHello method
+Student.prototype.sayHello = function(){
+  console.log('hi, I am a student');
+};
+
+// add sayGoodBye method
+Student.prototype.sayGoodBye = function(){
+  console.log('goodBye');
+};
+
+var student1 = new Student();
+student1.sayHello();
+student1.walk();
+student1.sayGoodBye();
+
+// check inheritance
+console.log(student1 instanceof Person); // true
+console.log(student1 instanceof Student); // true
