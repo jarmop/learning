b23705c478b3f3fe807e5db60edb252516bfc9e2
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-05-29 23:04:41 +0300

    Add todo.

--- web/custom/todo-mvc-vanilla/app.js
+++ web/custom/todo-mvc-vanilla/app.js
@@ -1,6 +1,33 @@
-(function (window) {
-	'use strict';
+/**
+ * Use cases:
+ * - DONE add todo
+ * - remove todo
+ * - set todo completed
+ * - unset todo completed
+ * - show active
+ * - show completed
+ * - clear completed
+ */
 
-	// Your starting point. Enjoy the ride!
+const KEY_ENTER = 'Enter';
 
-})(window);
+let todoList = document.querySelector('.todo-list');
+
+const addTodo = (text) => {
+  let listItem = document.createElement('li');
+  listItem.innerHTML = `
+<div class="view">
+  <input class="toggle" type="checkbox">
+  <label>${text}</label>
+  <button class="destroy"></button>
+</div>
+`;
+  todoList.appendChild(listItem);
+};
+
+window.onkeypress = (event) => {
+  if (event.key === KEY_ENTER) {
+    addTodo(event.target.value);
+    event.target.value = '';
+  }
+};
