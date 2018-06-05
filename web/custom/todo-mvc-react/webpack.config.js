4a0db44d480d3cf41a23c364f908cc8cae5a8091
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-06-06 00:01:28 +0300

    Render with react.

--- web/custom/todo-mvc-react/webpack.config.js
+++ web/custom/todo-mvc-react/webpack.config.js
@@ -1,5 +1,25 @@
-module.exports = mode => ({
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        options: { presets: ['env'] }
+      },
+    ]
+  },
+  resolve: { extensions: ['*', '.js', '.jsx'] },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+  ],
   devServer: {
     open: true,
   },
-});
+};
