4eb03b7e17d342bef566f0eeea29fe70e34aced2
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2018-06-17 21:11:32 +0300

    Add react router and a basic setup for a code splitting example.

---
+++ web/tools/webpack/full/src/section.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class Section extends React.Component {
+  render() {
+    return (
+        <div>A large section</div>
+    );
+  }
+}
+
+export default Section;
--- web/tools/webpack/full/webpack.config.js
+++ web/tools/webpack/full/webpack.config.js
@@ -18,4 +18,7 @@ module.exports = {
       template: 'src/index.html',
     }),
   ],
+  devServer: {
+    historyApiFallback: true,
+  }
 };
