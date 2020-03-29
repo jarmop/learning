3ec62494fd0956b9f3ba3c857c398ee5c20f7545
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2020-03-29 17:22:58 +0300

    Initial react lab.

---
+++ web/libraries/react/lab/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React list key</h1>
+    </div>
+  );
+}
+
+export default App;
