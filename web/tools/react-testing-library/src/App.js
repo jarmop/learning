77aa4befdf1796d13992898321b3c6f1d2460695
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2020-04-11 16:52:34 +0300

    Initial app for learning react testing library

---
+++ web/tools/react-testing-library/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      testing react testing library
+    </div>
+  );
+}
+
+export default App;
