964b50b0683b6693f382fa74c9539a58286eccd1
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2020-04-12 12:40:03 +0300

    Add/fix tests.

--- web/tools/react-testing-library/src/App.test.js
+++ web/tools/react-testing-library/src/App.test.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders learn react link', () => {
+// Simple smoke test can be done with just ReactDOM
+it('renders without crashing', () => {
+  // document is available in jsdom?
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
+});
+
+it('renders learn react link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/testing react testing library/i);
   expect(linkElement).toBeInTheDocument();
 });
