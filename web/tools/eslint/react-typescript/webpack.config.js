d2046ec58fb55ae228ba95a4c3f77cda8aaf045a
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2020-04-19 15:12:41 +0300

    Add npm script to run eslint. Ignore dist folder.

--- web/tools/eslint/react-typescript/webpack.config.js
+++ web/tools/eslint/react-typescript/webpack.config.js
@@ -1,12 +1,12 @@
 module.exports = {
-  mode: "production",
+  mode: 'production',
 
   // Enable sourcemaps for debugging webpack's output.
-  devtool: "source-map",
+  devtool: 'source-map',
 
   resolve: {
     // Add '.ts' and '.tsx' as resolvable extensions.
-    extensions: [".ts", ".tsx"]
+    extensions: ['.ts', '.tsx'],
   },
 
   module: {
@@ -16,17 +16,17 @@ module.exports = {
         exclude: /node_modules/,
         use: [
           {
-            loader: "ts-loader"
-          }
-        ]
+            loader: 'ts-loader',
+          },
+        ],
       },
       // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
       {
-        enforce: "pre",
+        enforce: 'pre',
         test: /\.js$/,
-        loader: "source-map-loader"
-      }
-    ]
+        loader: 'source-map-loader',
+      },
+    ],
   },
 
   // When importing a module whose path matches one of the following, just
@@ -34,7 +34,7 @@ module.exports = {
   // This is important because it allows us to avoid bundling all of our
   // dependencies, which allows browsers to cache those libraries between builds.
   externals: {
-    "react": "React",
-    "react-dom": "ReactDOM"
-  }
-};
+    react: 'React',
+    'react-dom': 'ReactDOM',
+  },
+}
