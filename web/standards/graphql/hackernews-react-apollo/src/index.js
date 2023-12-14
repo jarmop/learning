14c85736e360f263483e86338e06d909c095f85b
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2023-12-14 11:42:20 +0200

    GraphQL tutorial upto Authentication

--- web/standards/graphql/hackernews-react-apollo/src/index.js
+++ web/standards/graphql/hackernews-react-apollo/src/index.js
@@ -2,25 +2,35 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './styles/index.css'
 import App from './components/App'
-
-// 1
+import { BrowserRouter } from 'react-router-dom'
 import { ApolloProvider, ApolloClient, createHttpLink, InMemoryCache } from '@apollo/client'
+import { setContext } from '@apollo/client/link/context'
+import { AUTH_TOKEN } from './constants'
 
-// 2
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : '',
+    },
+  }
+})
+
 const httpLink = createHttpLink({
   uri: 'http://localhost:4000',
 })
 
-// 3
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 })
 
-// 4
 ReactDOM.render(
-  <ApolloProvider client={client}>
-    <App />
-  </ApolloProvider>,
+  <BrowserRouter>
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
+  </BrowserRouter>,
   document.getElementById('root')
 )
