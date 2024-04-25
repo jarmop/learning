f0d74ab4861b5e3d89f660134ae24a545a73c70c
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2024-04-25 14:56:29 +0300

    Simple Zustand example

--- web/libraries/zustand/zustand-app/src/App.tsx
+++ web/libraries/zustand/zustand-app/src/App.tsx
@@ -1,7 +1,24 @@
 import './App.css'
+import { useStore } from './store'
+
+
+function BearCounter() {
+  const bears = useStore((state) => state.bears)
+  return <h1>{bears} around here...</h1>
+}
+
+function Controls() {
+  const increasePopulation = useStore((state) => state.increasePopulation)
+  return <button onClick={increasePopulation}>one up</button>
+}
 
 function App() {
-  return <div>blank app</div>
+  return (
+    <div>
+      <BearCounter />
+      <Controls />
+    </div>
+  )
 }
 
 export default App
