8cfe06f8e8e52a74e62d2e9e8f14a1efe9573e06
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2025-12-03 16:43:35 +0200

    Fix GTK makefile

--- c/graphics/gtk/hello-world-gtk.c
+++ c/graphics/gtk/hello-world-gtk.c
@@ -1,27 +1,26 @@
 #include <gtk/gtk.h>
 
-static void print_hello(GtkWidget *widget, gpointer   data)
-{
+static void print_hello(GtkWidget *widget, gpointer   data) {
   g_print("Hello World\n");
 }
 
-static void activate(GtkApplication *app, gpointer user_data)
-{
+static void activate(GtkApplication *app, gpointer user_data) {
   GtkWidget *window = gtk_application_window_new(app);
   /* GTK_WINDOW(window) is the same as the standard C cast operator 
     "(GtkWindow*) window" but does some extra checking */
   gtk_window_set_title(GTK_WINDOW(window), "Hekjllo");
-  gtk_window_set_default_size(GTK_WINDOW(window), 200, 200);
+  gtk_window_set_default_size(GTK_WINDOW(window), 600, 400);
 
-  GtkWidget *button = gtk_button_new_with_label("Hello World");
+  GtkWidget *button = gtk_button_new_with_label("Hello World");  
+  gtk_widget_set_halign(button, GTK_ALIGN_CENTER);
+  gtk_widget_set_valign(button, GTK_ALIGN_CENTER);
   g_signal_connect(button, "clicked", G_CALLBACK(print_hello), NULL);
   gtk_window_set_child(GTK_WINDOW(window), button);
 
   gtk_window_present(GTK_WINDOW(window));
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
   GtkApplication *app = gtk_application_new(
     "org.gtk.example", 
     G_APPLICATION_DEFAULT_FLAGS
