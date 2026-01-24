0570ecb9efa0cdcf4efbd97e9a04eb0fa379060c
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2026-01-24 16:31:41 +0200

    C graphics: OpenGL + Wayland + EGL

---
+++ c/graphics/opengl/wayland-egl/generate-wayland.sh
@@ -0,0 +1,7 @@
+wayland-scanner client-header \
+  /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml \
+  xdg-shell-client-protocol.h
+
+wayland-scanner private-code \
+  /usr/share/wayland-protocols/stable/xdg-shell/xdg-shell.xml \
+  xdg-shell-protocol.c
