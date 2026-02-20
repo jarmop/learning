b5d2d0fbc958ace7ae76255f83891c66a38a4d15
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2026-02-20 11:52:12 +0200

    Zig

--- zig/hello.zig
+++ zig/hello.zig
@@ -1,5 +1,5 @@
 const std = @import("std");
 
-pub fn main() !void {
-    try std.fs.File.stdout().writeAll("Hello, World!\n");
+pub fn main() void {
+    std.debug.print("Hello, {s}!\n", .{"World"});
 }
