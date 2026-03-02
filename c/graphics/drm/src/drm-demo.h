bd3fad409384abd8ddfd1fa10e3ee68f2be34f68
Jarmo Pihlajaniemi <jarmopih@gmail.com>
2026-03-02 14:56:11 +0200

    DRM: Cleanup

--- c/graphics/drm/src/drm-demo.h
+++ c/graphics/drm/src/drm-demo.h
@@ -7,4 +7,3 @@ void drawGradient(uint16_t width, uint16_t height, uint8_t *map, uint32_t pitch)
 void drawCheckers(uint16_t width, uint16_t height, uint8_t *map, uint32_t pitch);
 void drawPlain(uint16_t width, uint16_t height, uint8_t *map, uint32_t pitch);
 void drawMouse(uint16_t mouse_x, uint16_t mouse_y, uint8_t *map, uint32_t pitch);
-void mouse();
